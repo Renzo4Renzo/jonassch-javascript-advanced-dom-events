a0157e2b44de028e697b17182383f44af5143338
Renzo Diaz <renzodiaz554@gmail.com>
2023-05-01 12:59:50 -0500

    Implementing Smooth Scrolling

--- script.js
+++ script.js
@@ -30,85 +30,32 @@ document.addEventListener('keydown', function (event) {
   }
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (event) {
+  const section1Coords = section1.getBoundingClientRect();
+  console.log(section1Coords);
+
+  // console.log(event.target.getBoundingClientRect());
+
+  // console.log('Current scroll (X/Y):', window.pageXOffset, window.pageYOffset);
+  // console.log(
+  //   'Width/Height viewport:',
+  //   document.documentElement.clientWidth,
+  //   document.documentElement.clientHeight
+  // );
+  // window.scrollTo(
+  //   section1Coords.left + window.pageXOffset,
+  //   section1Coords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: section1Coords.left + window.pageXOffset,
+  //   top: section1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 ////////////////////////////// LECTURES //////////////////////////////
-
-// Selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
-
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section'); //Retrives a NodeList, which is NOT updated live
-console.log(allSections);
-
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button'); //Retrieves an HTMLCollection, which is updated live
-console.log(allButtons);
-
-const allBtns = document.getElementsByClassName('btn');
-console.log(allBtns);
-
-// Creating and inserting elements
-// .insertAdjacentHTML
-
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML = `We use biscuits for improvements functionality and analytics.
-  <button class="btn btn--close-cookie">Got it!</button>`;
-
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.after(message);
-
-//Delete elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-    // message.parentElement.removeChild(message);
-  });
-
-//Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-console.log(message.style.height);
-console.log(message.style.backgroundColor); //Only works for styles we defined
-
-console.log(getComputedStyle(message).height);
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
-document.documentElement.style.setProperty('--color-primary', 'peru');
-
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.className);
-logo.alt = 'Beatiful minimalist logo';
-
-console.log(logo.designer); //Doesn't work
-console.log(logo.getAttribute('designer')); //Works!
-logo.setAttribute('company', 'Bankist');
-
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
-
-const link = document.querySelector('.nav__link--btn');
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-//Data attributes
-console.log(logo.dataset.versionRenzo);
-
-// Classes
-logo.classList.add('renzo-class');
-logo.classList.remove('renzo-class');
-logo.classList.toggle('renzo-class');
-console.log(logo.classList.contains('renzo-class'));
-
-logo.className = 'renzo'; //This override all existing classes only putting 1
