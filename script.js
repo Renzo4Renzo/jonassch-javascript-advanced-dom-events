2228ba6b27571c5d64defdb7aaf5070116aa7675
Renzo Diaz <renzodiaz554@gmail.com>
2023-05-04 23:35:01 -0500

    DOM Traversing

--- script.js
+++ script.js
@@ -33,15 +33,6 @@ document.addEventListener('keydown', function (event) {
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
-//Page navigation
-// document.querySelectorAll('.nav__link').forEach(element => {
-//   element.addEventListener('click', function (event) {
-//     event.preventDefault();
-//     const navigationId = this.getAttribute('href');
-//     document.querySelector(navigationId).scrollIntoView({ behavior: 'smooth' });
-//   });
-// });
-
 document
   .querySelector('.nav__links')
   .addEventListener('click', function (event) {
@@ -67,3 +58,29 @@ btnScrollTo.addEventListener('click', function () {
 });
 
 ////////////////////////////// LECTURES //////////////////////////////
+const h1 = document.querySelector('h1');
+
+//Going downwards: children
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'peru';
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(element => {
+  if (element !== h1) element.style.transform = 'scale(0.5)';
+});
