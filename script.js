552c5469b79551283d1fb16486021cc67c0f4adf
Renzo Diaz <renzodiaz554@gmail.com>
2023-05-06 13:21:13 -0500

    Building a Tabbed Component

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const navBar = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// Modal window
 const openModal = function (event) {
   event.preventDefault();
   modal.classList.remove('hidden');
@@ -57,30 +62,19 @@ btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
 
-////////////////////////////// LECTURES //////////////////////////////
-const h1 = document.querySelector('h1');
+// Tabbed component
+tabsContainer.addEventListener('click', function (event) {
+  const tabClicked = event.target.closest('.operations__tab');
+  if (!tabClicked) return; //Guard clause
 
-//Going downwards: children
-console.log(h1.querySelectorAll('.highlight'));
-console.log(h1.childNodes);
-console.log(h1.children);
-h1.firstElementChild.style.color = 'white';
-h1.lastElementChild.style.color = 'peru';
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabClicked.classList.add('operations__tab--active');
 
-//Going upwards: parents
-console.log(h1.parentNode);
-console.log(h1.parentElement);
-h1.closest('.header').style.background = 'var(--gradient-secondary)';
-h1.closest('h1').style.background = 'var(--gradient-primary)';
-
-//Going sideways: siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
-
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
-
-console.log(h1.parentElement.children);
-[...h1.parentElement.children].forEach(element => {
-  if (element !== h1) element.style.transform = 'scale(0.5)';
+  tabsContent.forEach(tabContent =>
+    tabContent.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${tabClicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
+////////////////////////////// LECTURES //////////////////////////////
